5b04e9a07e80596da30c677d667cff1d7e03c1bc
suresh vaadapalli <surehs@gmail.com>
2025-11-21 19:38:40 +0530

    msg

--- first.sh
+++ first.sh
@@ -9,7 +9,7 @@ do
     --image-id $AMI_ID \
     --instance-type t2.micro \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' --output text
 
     if [ $instance != "frontend" ]; then
