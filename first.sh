5f0169a17d44095ea7fab1d346640e4cba163526
suresh vaadapalli <surehs@gmail.com>
2025-11-21 19:30:32 +0530

    msg

--- first.sh
+++ first.sh
@@ -13,18 +13,18 @@ do
   --query "Reservations[0].Instances[0].InstanceId" \
   --output text)
 
-  if [ $instance != "frontend" ]; then
-  IP=$(aws ec2 describe-instances \
-  --instance-ids $INSTANCE_ID \
-  --query "Reservations[0].Instances[0].PrivateIpAddress" \
-  --output text)
-  else
-  IP=$(aws ec2 describe-instances \
-  --instance-ids $INSTANCE_ID \
-  --query "Reservations[0].Instances[0].PublicIpAddress" \
-  --output text)
-  fi
+if [ $instance != "frontend" ]; then
+IP=$(aws ec2 describe-instances \
+--instance-ids $INSTANCE_ID \
+--query "Reservations[0].Instances[0].PrivateIpAddress" \
+--output text)
+else
+IP=$(aws ec2 describe-instances \
+--instance-ids $INSTANCE_ID \
+--query "Reservations[0].Instances[0].PublicIpAddress" \
+--output text)
+fi
 
-  echo "$instance: $IP"
+echo "$instance: $IP"
 
 done
