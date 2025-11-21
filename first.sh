c6be92d7de65099533106b9f960b3b115d666e12
suresh vaadapalli <surehs@gmail.com>
2025-11-21 19:36:38 +0530

    msg

--- first.sh
+++ first.sh
@@ -5,25 +5,25 @@ SG_ID="sg-0c8311807a45cafaa"
 
 for instance in $@
 do
-  INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type t2.micro \
-  --security-group-ids $SG_ID \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
-  --query "Reservations[0].Instances[0].InstanceId" \
-  --output text)
-
-  if [ $instance != "frontend" ]; then
-    IP=$(aws ec2 describe-instances \
-    --instance-ids $INSTANCE_ID \
-    --query "Reservations[0].Instances[0].PrivateIpAddress" \
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t2.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
+    --query "Reservations[0].Instances[0].InstanceId" \
     --output text)
-  else
-    IP=$(aws ec2 describe-instances \
-    --instance-ids $INSTANCE_ID \
-    --query "Reservations[0].Instances[0].PublicIpAddress" \
-    --output text)
-  fi
 
-  echo "$instance: $IP"
+    if [ $instance != "frontend" ]; then
+      IP=$(aws ec2 describe-instances \
+      --instance-ids $INSTANCE_ID \
+      --query "Reservations[0].Instances[0].PrivateIpAddress" \
+      --output text)
+    else
+       IP=$(aws ec2 describe-instances \
+      --instance-ids $INSTANCE_ID \
+      --query "Reservations[0].Instances[0].PublicIpAddress" \
+      --output text)
+    fi
+
+    echo "$instance: $IP"
 done
