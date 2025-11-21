2f620dd8788e06d9de017185d3e38e68012ae235
suresh vaadapalli <surehs@gmail.com>
2025-11-21 21:13:00 +0530

    msg

--- first.sh
+++ first.sh
@@ -15,11 +15,13 @@ do
       --instance-ids $INSTANCE_ID \
       --query "Reservations[0].Instances[0].PrivateIpAddress" \
       --output text)
+      
     else
        IP=$(aws ec2 describe-instances \
       --instance-ids $INSTANCE_ID \
       --query "Reservations[0].Instances[0].PublicIpAddress" \
       --output text)
+      
     fi
 
     echo "$instance: $IP"
