37cec6e3485f5b8b7298b2a8f76a9c75a0436e50
suresh vaadapalli <surehs@gmail.com>
2025-11-21 19:29:19 +0530

    msg

--- first.sh
+++ first.sh
@@ -9,17 +9,17 @@ do
   --image-id $AMI_ID \
   --instance-type t2.micro \
   --security-group-ids $SG_ID \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
   --query "Reservations[0].Instances[0].InstanceId" \
   --output text)
 
-  if [ $instance -ne "frontend" ]; then
+  if [ $instance != "frontend" ]; then
   IP=$(aws ec2 describe-instances \
   --instance-ids $INSTANCE_ID \
   --query "Reservations[0].Instances[0].PrivateIpAddress" \
   --output text)
   else
-    IP=$(aws ec2 describe-instances \
+  IP=$(aws ec2 describe-instances \
   --instance-ids $INSTANCE_ID \
   --query "Reservations[0].Instances[0].PublicIpAddress" \
   --output text)
